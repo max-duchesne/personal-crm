f16f28cbee0afd52b8308d5cb02390358aeec8a8
Max Duchesne <max.duchesne@gmail.com>
2023-07-20 18:36:56 -0500

    changed settings to test on ios

--- backend/personal_crm/settings.py
+++ backend/personal_crm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s40lrc3hh24=fh3&cacnda@e10j_m7*2ww5&+z&e#gt&w_bdn3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.71']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,4 +1,4 @@
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'http://192.168.0.71:8000';
 
 export const fetchContacts = async () => {
   try {
