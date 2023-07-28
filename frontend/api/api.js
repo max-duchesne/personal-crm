09c053dd6d5bfd0a14c635189f5500c7d39965d2
Max Duchesne <max.duchesne@gmail.com>
2023-07-28 01:07:11 -0500

    added auth for create, update, delete operations

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -28,3 +28,75 @@ export const fetchContacts = async () => {
     throw error;
   }
 };
+
+export const createContact = async (newContact) => {
+  const token = await getAccessToken();
+  
+  const response = await fetch(`${API_URL}/contacts/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(newContact)
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error('Failed to create new contact');
+};
+
+export const updateContact = async (updatedContact) => {
+  const token = await getAccessToken();
+  
+  const response = await fetch(`${API_URL}/contacts/${updatedContact.id}/`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(updatedContact)
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error('Failed to update contact');
+};
+
+export const deleteContact = async (id) => {
+  const token = await getAccessToken();
+
+  const response = await fetch(`${API_URL}/contacts/${id}/`, {
+    method: 'DELETE',
+    headers: {
+      'Authorization': `Bearer ${token}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to delete contact');
+  }
+};
+
+export const createInteraction = async (newInteraction) => {
+  const token = await getAccessToken();
+  
+  const response = await fetch(`${API_URL}/interactions/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${token}`
+    },
+    body: JSON.stringify(newInteraction)
+  });
+
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error('Failed to create new interaction');
+};
